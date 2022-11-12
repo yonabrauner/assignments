dc19ca23883cbfa224714865655d662ab34b90a6
roneitan <118009479+roneitan@users.noreply.github.com>
2022-11-12 16:35:02 +0200

    Add files via upload

--- Ass_1/src/Coalition.cpp
+++ Ass_1/src/Coalition.cpp
@@ -1,13 +1,41 @@
-#include <vector>
+// #include <vector>
 #include "Coalition.h"
 
+
+//constructor
+Coalition::Coalition(){
+    col = new vector<int>
+    mMandates = new int(0);
+}
 Coalition::Coalition(int partyID) {
-    vector<int> col;
+    mMandates = new int(0);
+    col = new vector<int>
     col.push_back(partyID);
 }
-
 //destructor
-
+Coalition::~Coalition(){delete col;delete mMandates}
 //copy constructor
-
+Coalition:: Coalition(Coalition &other){
+        this = new Coalition();
+        this = other;
+}
 //copy assignment operator
+Coalition::Coalition &operator=(const Coalition &other){
+    if(this != other){
+        this.mMandates = other.getNumMandates();
+        *col = *(other.getCoalitionVector());
+    }
+}
+vector<int> Coalition::getCoalitionVector(){
+    // return col
+}
+
+int Coalition::getNumMandates(){
+    return mMandates;
+}
+
+void Coalition::addToCoaltion(int partyID){
+    //push the id into coalition
+    //update mMandates
+}
+
